fa942d48bdd62622132085c8aec6c0d20e118b90
jl <jl@axin.fr>
2017-09-12 14:06:29 +0200

    creation d'une route avec wildcard, mise en place d'un state cumulatif

---
+++ src/components/post_show.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class PostShow extends Component {
+
+    render(){
+
+        return (
+            <div>
+                Post shows
+            </div>
+        );
+    }
+}
+
+export default PostShow;
