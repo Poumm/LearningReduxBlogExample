149ebe3f34c09516c7973b6ceaef653fd8c7ec29
Jerome Lalisse <jerome.lalisse@hotmail.fr>
2017-09-12 23:07:22 +0200

    creation d'un state base sur un fetch element et récuperation d information depuis url

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,15 +1,48 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { fetchPost } from '../actions';
+
 
 class PostShow extends Component {
 
+    componentDidMount(){
+        //params contient toutes les wildcard de notre url
+        //tout ce que contient match est fourni pas redux-router
+        const {id} = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
     render(){
+
+        const {post} = this.props;
+        //console.log(post);
+        if(!post){
+            return <div>Loading...</div>;
+        }
 
         return (
             <div>
-                Post shows
+                <h3>{post.title}</h3>
+                <h6>Categories : {post.categories}</h6>
+                <p>{post.content}</p>
+                <div className="text-xs-right">
+                    <Link to="/" className="btn btn-primary">Back</Link>
+                    <button className="btn btn-danger">Delete</button>
+                </div>
             </div>
         );
     }
 }
 
-export default PostShow;
+function mapStateToProps( {posts}, ownProps) {
+    //console.log(posts);
+    if(!posts){
+        return {state : null};
+    } 
+    //console.log(posts[ownProps.match.params.id]);
+    return { post : posts[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps,  {fetchPost  })(PostShow);
